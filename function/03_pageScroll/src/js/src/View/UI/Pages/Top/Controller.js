d2d2c17af5b88aee5d6a14ed1d96d757dddf20d8
Eri_Tsutsui <tsutsui@balance.bz>
2021-12-24 11:58:57 +0900

    ページ内スクロール途中

---
+++ function/03_pageScroll/src/js/src/View/UI/Pages/Top/Controller.js
@@ -0,0 +1,36 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
+
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+
+import gsap from "gsap";
+import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
+
+import { Conf } from "@/Conf";
+
+import PageScroll from "./PageScroll/Controller";
+
+export default class Controller extends Base {
+  constructor() {
+    super();
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {
+    new PageScroll();
+  }
+
+  update() {}
+
+  onResize() {}
+
+  setEvents() {
+    super.setEvents();
+  }
+}
---
+++ function/03_pageScroll/src/js/src/View/UI/Pages/Top/PageScroll/Controller.js
@@ -0,0 +1,77 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
+
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+import gsap from "gsap";
+import { UAParser } from "ua-parser-js";
+import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
+
+export default class Controller extends Base {
+  constructor() {
+    super();
+
+    this.isREv = true;
+
+    this.name = "PageScroll";
+
+    this.$btn = $(".js-pagescroll_btn");
+    this.$html = $("html");
+
+    this.$lastsection = $(".js-pagescroll_lastsection");
+    this.st = 0;
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {
+    this.calcBuffa();
+  }
+
+  onResize() {
+    this.calcBuffa();
+  }
+
+  calcBuffa() {
+    // スクロール量
+    this.st = $(window).scrollTop();
+
+    // contact topまでの高さ
+    const top = this.$lastsection.get(0).getBoundingClientRect().top + this.st;
+    const wholeHeight = document.body.clientHeight;
+
+    // buffa
+    this.buffa = window.innerHeight - (wholeHeight - top);
+  }
+
+  onClick(btn) {
+    const href = $(btn).attr("href");
+    const $targetPosEle = $(href);
+
+    let targetTop;
+
+    if (href == "#contact") {
+      targetTop = $targetPosEle.offset().top - this.buffa;
+    } else {
+      targetTop = $targetPosEle.offset().top;
+    }
+
+    gsap.to(this.$html, 1, {
+      scrollTop: targetTop,
+      ease: "power4.out",
+    });
+  }
+
+  setEvents() {
+    super.setEvents();
+
+    this.$btn.on("click" + "." + this.name, (e) => {
+      e.preventDefault();
+      this.onClick(e.currentTarget);
+    });
+  }
+}
