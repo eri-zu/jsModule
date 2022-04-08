9d788e622ce7bde4d2548e51adf692d6922e5952
Eri_Tsutsui <tsutsui@balance.bz>
2022-04-08 20:36:36 +0900

    fix: contactコーディング

--- function/16_contact/01_simple/src/js/src/View/UI/Pages/Contact/Validation/Controller.js
+++ function/16_contact/01_simple/src/js/src/View/UI/Pages/Contact/Validation/Controller.js
@@ -7,10 +7,6 @@
 import Base from "@BALANCeLibs/Base.js";
 import * as m from "@BALANCeLibs/Util/Math.js";
 
-import gsap from "gsap";
-// import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
-import { Conf } from "@/Conf";
-
 export default class Controller extends Base {
   constructor() {
     super();
